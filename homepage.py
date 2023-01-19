042d42cf6e44c0f7532d7998c40e81babd5b35a5
Shivayavashi <110244010+Shivayavashi@users.noreply.github.com>
2023-01-19 19:49:20 +0530

    Update and rename demo.py to homepage.py

---
+++ homepage.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.balloons()
+st.snow()
+st.title("Welcome to Homepage")
+st.header("Student marks visualization")
+st.write("Go to form section and fill the form and upload it to get your visualization")
+st.header('Hello 🌎!')
