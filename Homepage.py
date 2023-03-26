8128a1e0f0da720ff24a2bdd9d0fa47284875d90
Shivayavashi <110244010+Shivayavashi@users.noreply.github.com>
2023-03-26 19:26:49 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,6 +1,4 @@
 import streamlit as st
-st.balloons()
-st.snow()
 st.title("Welcome to Homepage")
 st.header("Student marks visualization")
 st.write("Go to form section and fill the form and upload it to get your visualization")
